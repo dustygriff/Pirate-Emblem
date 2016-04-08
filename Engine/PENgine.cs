490beda89530e9acdfb2e7a12ca0bf52c1cad3bb
jmacattack22 <jason.mcdonald11@gmail.com>
2016-04-08 16:33:18 -0230

    Testing adding to Project

---
+++ Engine/PENgine.cs
@@ -0,0 +1,21 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jason
+ * Date: 4/8/2016
+ * Time: 2:40 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+
+namespace Engine
+{
+	/// <summary>
+	/// Description of MyClass.
+	/// </summary>
+	public class PEngine
+	{
+		
+	}
+}
---
+++ Engine/Unit.cs
@@ -0,0 +1,22 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jason
+ * Date: 4/8/2016
+ * Time: 3:16 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Engine
+{
+	/// <summary>
+	/// Description of Unit.
+	/// </summary>
+	public class Unit
+	{
+		public Unit()
+		{
+		}
+	}
+}
