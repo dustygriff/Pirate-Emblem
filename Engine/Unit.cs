9496fcbb217e4b88a83455aa36e0a85580659d2a
jmacattack22 <jason.mcdonald11@gmail.com>
2016-04-08 20:12:34 -0230

    Added variables and Setters

--- Engine/Unit.cs
+++ Engine/Unit.cs
@@ -11,16 +11,100 @@ using System;
 namespace Engine
 {
 	/// <summary>
-	/// Description of Unit.
+	/// Unit class contains all necessary information about crewmember pertaining to combat as well as functionality to draw the sprite.
 	/// </summary>
 	public class Unit
 	{
 		
 		#region General Stats
+		public UInt16 Constitution_;
+		public UInt16 Defense_;
 		public UInt16 Health_;
+		public UInt16 Level_; //Is an Accumulating number i.e 456 = Level 4 with 56/100 experience
+		public UInt16 Luck_;
+		public UInt16 Movement_;
+		public UInt16 Speed_;
+		public UInt16 Skill_;
+		public UInt16 Strength_;
 		#endregion
-		public Unit()
+		
+		#region Weapon Stats
+		public UInt16 AxeLvl_;
+		public UInt16 BowLvl_;
+		public UInt16 GunLvl_;
+		public UInt16 PikeLvl_;
+		public UInt16 SwordLvl_;
+		#endregion
+		
+		
+		
+		public Unit(
+			UInt16 con,
+			UInt16 def,
+			UInt16 hp,
+			UInt16 lvl,
+			UInt16 lck,
+			UInt16 mov,
+			UInt16 spd,
+			UInt16 skl,
+			UInt16 str)
 		{
+			Constitution_ = con;
+			Defense_ = def;
+			Health_ = hp;
+			Level_ = lvl;
+			Luck_ = lck;
+			Movement_ = mov;
+			Speed_ = spd;
+			Skill_ = skl;
+			Strength_ = str;
 		}
+		
+		#region Setters
+		public void SetConstitution(UInt16 con)
+		{
+			Constitution_ = con;
+		}
+		
+		public void SetDefense(UInt16 def)
+		{
+			Defense_ = def;
+		}
+		
+		public void SetHealth(UInt16 hp)
+		{
+			Health_ = hp;
+		}
+		
+		public void SetLevel(UInt16 lvl)
+		{
+			Level_ = lvl;
+		}
+		
+		public void SetLuck(UInt16 lck)
+		{
+			Luck_ = lck;
+		}
+		
+		public void SetMovement(UInt16 mov)
+		{
+			Movement_ = mov;
+		}
+		
+		public void SetSpeed(UInt16 spd)
+		{
+			Speed_ = spd;
+		}
+		
+		public void SetSkill(UInt16 skl)
+		{
+			Skill_ = skl;
+		}
+		
+		public void SetStrength(UInt16 str)
+		{
+			Strength_ = str;
+		}
+		#endregion
 	}
 }
